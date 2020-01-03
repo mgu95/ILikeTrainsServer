d9cdf5f8b00cce5793f01dbdbf915cef3152987f
mgu95 <mariuszgusc@outlook.com>
2020-01-03 21:27:32 +0100

    Working Board class;

--- src/game/engine/Board.java
+++ src/game/engine/Board.java
@@ -6,17 +6,9 @@ import java.util.*;
 
 public class Board {
 
-    //zmienne potrzebne do nowego boarda:
     private long id;
     private Field[][] fields;
     private Player[] players;
-
-    public static void main(String[] args) {
-        Board board = new Board(1, 20, 26, 5);
-        board.getDefaultBoard();
-
-        board.print();
-    }
 
     public Board(long id, int height, int width, int numberOfPlayers) {
         this.id = id;
@@ -31,8 +23,18 @@ public class Board {
         }
     }
 
-    public Board getDefaultBoard() {
-        Board board = new Board(1, 20, 30, 5);
+    public Board(long id) {
+        this.id = id;
+        this.fields = new Field[26][20];
+        this.players = new Player[5];
+        long fieldId = 1;
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < 20; j++) {
+                fields[i][j] = new Field(fieldId, i, j);
+                fieldId++;
+            }
+        }
+
         setCity(fields[0][0], 1, "Tirana");
         setCity(fields[6][0], 2, "Andora");
         setCity(fields[12][0], 3, "Wiedeń");
@@ -72,28 +74,65 @@ public class Board {
         setCity(fields[22][19], 37, "Rzeszów");
 
         setRoute(1, "ORANGE", 1, 2);
-        //tutaj zolta trasa
-        setRoute(2, "RED", 1, 8);
-
-
-
-
-
-
-
-
-        return board;
-    }
-
-    private Field getField(long fieldId) {
-        for(Field[] fields : getFields()) {
-            for (Field field : fields) {
-                if (field.getFieldId() == fieldId) {
-                    return field;
-                }
-            }
-        }
-        throw new NullPointerException("There is no field with id " + fieldId + "!");
+        setRoute(2, "YELLOW", 1, 10);
+        setRoute(3, "RED", 1, 8);
+        setRoute(4, "BLUE", 2, 3);
+        setRoute(5, "ORANGE", 2, 11);
+        setRoute(6, "GREY", 3, 4);
+        setRoute(7, "PINK", 3, 7);
+        setRoute(8, "RED", 4, 9);
+        setRoute(9, "GREY", 5, 6);
+        setRoute(10, "BLUE", 5, 12);
+        setRoute(11, "PINK", 5, 9);
+        setRoute(12, "RED", 6, 12);
+        setRoute(13, "ORANGE" ,7, 13);
+        setRoute(14, "YELLOW", 7, 11);
+        setRoute(15, "RED", 8, 15);
+        setRoute(16, "PINK", 8, 19);
+        setRoute(17, "YELLOW", 9, 14);
+        setRoute(18, "GREY", 9, 21);
+        setRoute(19, "YELLOW", 9, 13);
+        setRoute(20, "BLUE", 10, 11);
+        setRoute(21, "YELLOW", 10, 15);
+        setRoute(22, "PINK", 11,16);
+        setRoute(23, "GREY", 12, 14);
+        setRoute(24, "RED", 13, 17);
+        setRoute(25, "RED", 14, 18);
+        setRoute(26, "PINK", 14, 22);
+        setRoute(27, "GREY", 15,23);
+        setRoute(28, "PINK", 15, 25);
+        setRoute(29, "GREY", 15, 19);
+        setRoute(30, "GREY", 16, 20);
+        setRoute(31, "BLUE", 16, 23);
+        setRoute(32, "ORANGE", 17, 21);
+        setRoute(33, "BLUE", 17, 20);
+        setRoute(34, "BLUE", 18, 30);
+        setRoute(35, "ORANGE", 18, 22);
+        setRoute(36, "RED", 19, 24);
+        setRoute(37, "YELLOW", 20, 27);
+        setRoute(38, "BLUE", 21, 22);
+        setRoute(39, "RED", 21, 36);
+        setRoute(40, "PINK", 21, 28);
+        setRoute(41, "YELLOW", 22, 29);
+        setRoute(42, "RED", 23, 26);
+        setRoute(43, "YELLOW", 24, 25);
+        setRoute(44, "PINK", 24, 33);
+        setRoute(45, "BLUE", 25, 26);
+        setRoute(46, "ORANGE", 25, 31);
+        setRoute(47, "GREY", 25, 34);
+        setRoute(48, "BLUE", 25, 33);
+        setRoute(49, "ORANGE", 26, 27);
+        setRoute(50, "GREY", 26, 32);
+        setRoute(51, "YELLOW", 26, 31);
+        setRoute(52, "RED", 27, 32);
+        setRoute(53, "BLUE", 28, 35);
+        setRoute(54, "BLUE", 29, 30);
+        setRoute(55, "ORANGE", 29, 37);
+        setRoute(56, "PINK", 31, 32);
+        setRoute(57, "ORANGE", 33, 34);
+        setRoute(58, "ORANGE", 35, 36);
+        setRoute(59, "PINK", 36, 37);
+        setRoute(60, "YELLOW", 28, 32);
     }
 
     private City getCity(long cityId) {
@@ -108,6 +147,18 @@ public class Board {
             }
         }
         throw new NullPointerException("There is no city with id " + cityId + "!");
+    }
+
+    private Set<City> getAllCities() {
+        Set<City> set = new HashSet<>();
+        for(Field[] fields : fields) {
+            for (Field field : fields) {
+                if (field.getClass() == City.class) {
+                    set.add((City) field);
+                }
+            }
+        }
+        return set;
     }
 
     private void setCity(Field field, long cityId, String name) {
@@ -129,6 +180,18 @@ public class Board {
         throw new NullPointerException("There is no route with id " + routeId + "!");
     }
 
+    private Set<Route> getAllRoutes() {
+        Set<Route> set = new HashSet<>();
+        for(Field[] fields : fields) {
+            for (Field field : fields) {
+                if (field.getClass() == Route.class) {
+                    set.add((Route) field);
+                }
+            }
+        }
+        return set;
+    }
+
     private void setRoute(long routeId, String color, long fromCityId, long toCityId) {
         City cityA = getCity(fromCityId);
         City cityB = getCity(toCityId);
@@ -143,14 +206,33 @@ public class Board {
                 fields[cityA.getXPosition()][i] = new Route(fields[cityA.getXPosition()][i], routeId, color);
             }
         } else {
-            // implementacja szukania miasta na ukos
+            City highestCity;
+            if (cityA.getYPosition() < cityB.getYPosition()) { highestCity = cityA; }
+            else { highestCity = cityB; }
+            City cityOnLeft;
+            if (cityA.getXPosition() < cityB.getXPosition()) { cityOnLeft = cityA; }
+            else { cityOnLeft = cityB; }
+            if (highestCity.equals(cityA) && cityOnLeft.equals(cityA)) {
+                int y = cityB.getYPosition() - cityA.getYPosition();
+                for (int i = 1; i < y; i++) {
+                    fields[cityA.getXPosition() + i][cityA.getYPosition() + i]
+                           = new Route(fields[cityA.getXPosition() + i][cityA.getYPosition() + i], routeId, color);
+                }
+            } else if (highestCity.equals(cityA) && cityOnLeft.equals(cityB)) {
+                int y = cityB.getYPosition() - cityA.getYPosition();
+                for (int i = 1; i < y; i++) {
+                    fields[cityA.getXPosition() - i][cityA.getYPosition() + i]
+                            = new Route(fields[cityA.getXPosition() - i][cityA.getYPosition() + i], routeId, color);
+                }
+            }
+
         }
         fields[cityA.getXPosition()][cityA.getYPosition()] = cityA;
         fields[cityB.getXPosition()][cityB.getYPosition()] = cityB;
     }
 
     public void print() {
-        StringBuilder board = new StringBuilder("  ");
+        StringBuilder board = new StringBuilder("   ");
         StringBuilder cityLegend = new StringBuilder("Miasta:\n");
         StringBuilder routeLegend = new StringBuilder("Trasy:\n");
         Field[][] f = this.fields;
@@ -159,10 +241,10 @@ public class Board {
         }
         Set<Route> allRoutes = new HashSet<>();
         for (int i = 0; i < f[0].length; i++) {
-            if (i < 10) {
-                board.append("\n" + (i + 1) + " ");
+            if (i < 9) {
+                board.append("\n" + (i + 1) + "  ");
             } else {
-                board.append("\n" + (i + 1));
+                board.append("\n" + (i + 1) + " ");
             }
 
             for (int j = 0; j < f.length; j++) {
@@ -198,22 +280,26 @@ public class Board {
             }
         }
         for (int i = 1; i < highestRouteId + 1; i++) {
-            Set<Route> rSet = new HashSet<>();
-            for (Route route : allRoutes) {
-                if (route.getRouteId() == i) {
-                    rSet.add(route);
+            routeLegend.append(i + " (" + getRoute(i).getColor() + ") = ");
+            Set<City> allCitiesOnBoard = getAllCities();
+            for (City city : allCitiesOnBoard) {
+                long[] routesId = city.getRoutesId();
+                for (long l : routesId) {
+                    if (l == i) {
+                        routeLegend.append(city.getName() + " <-> ");
+                    }
                 }
             }
-            routeLegend.append(i + " (" + rSet.iterator().next().getColor() + ") = ");
-            for (Route route : rSet) {
-                routeLegend.append((char)(route.getYPosition() + 65) + "" + (route.getXPosition() + 1) + " ");
-            }
+            String tmp = routeLegend.substring(0, routeLegend.length() - 5);
+            routeLegend.setLength(0);
+            routeLegend.append(tmp);
             routeLegend.append("\n");
         }
         System.out.println(board.toString() + "\n\n" + cityLegend.toString() + "\n" + routeLegend.toString());
     }
 
-    //zmienne potrzebne do starego boarda:
+
+    // OLD BOARD
     private int width;
     private int height;
     private Set<City> cities = new HashSet<>();
